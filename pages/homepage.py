566d202e80455515a8ce63f833f12887ada4ee6b
Charlie <143125248+CarlosVillatoro1995@users.noreply.github.com>
2025-06-26 13:32:51 -0400

    created an instance of a blueprint

--- pages/homepage.py
+++ pages/homepage.py
@@ -1,5 +1,9 @@
 from flask import Blueprint
 
 
+homepage_bp = Blueprint("homepage", __name__)
 
+@homepage_bp.route("/") #function that wraps another function to add extra behavior to it (decorator) without changing the function itself
+def home():
+    return "homepage"
 
