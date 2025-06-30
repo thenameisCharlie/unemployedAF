99d9bc5c118a7f6b7c83276d1c17878bff4ac871
CarlosVillatoro <a7xcharlie18@gmail.com>
2025-06-29 21:47:10 -0400

    Linked homepage backend with front end

--- pages/homepage.py
+++ pages/homepage.py
@@ -1,12 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 
 
 homepage_bp = Blueprint("homepage", __name__)
 
 @homepage_bp.route("/") #function that wraps another function to add extra behavior to it (decorator) without changing the function itself
-def home():
-    return "homepage"
+def homepage():
+     
+
+
+
+
+    return render_template("home.html")
 
 
 
