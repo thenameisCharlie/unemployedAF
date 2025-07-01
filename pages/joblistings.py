32b8ebc5778f407fbd1e0647de8165d7a554796a
Charlie <143125248+CarlosVillatoro1995@users.noreply.github.com>
2025-07-01 17:27:48 -0400

    Added a query variable

--- pages/joblistings.py
+++ pages/joblistings.py
@@ -8,6 +8,11 @@ jobListing_bp = Blueprint("joblistings", __name__)
 @jobListing_bp.route("/search")
 def search_results():
     title = request.args.get("title", "all")
+    location = request.args.get("location", "all")
+    site = request.args.get("site", "all")
+
+    query = f"{title} jobs in {location} via {site}"
+
 
 
 
