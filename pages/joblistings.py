50c95093b16225192791b717f7af1518b49a864f
CarlosVillatoro <a7xcharlie18@gmail.com>
2025-07-01 22:58:11 -0400

    creating the backend API request

--- pages/joblistings.py
+++ pages/joblistings.py
@@ -1,17 +1,27 @@
 from flask import Blueprint, render_template, request
 
 
-
 jobListing_bp = Blueprint("joblistings", __name__)
 
-
+#Provides the results of the homepage search bar 
 @jobListing_bp.route("/search")
 def search_results():
+
+    #Reads the data from the html form the user sends the backend
     title = request.args.get("title", "all")
     location = request.args.get("location", "all")
     site = request.args.get("site", "all")
 
+    #Sends request from backend to another server
     query = f"{title} jobs in {location} via {site}"
+
+    url = "https://jsearch.p.rapidapi.com/search"
+
+    params = {"query": query}
+
+    headers = {"x-rapidapi-key": API_KEY, "x-rapidapi-host": "jsearch.p.rapidapi.com"}
+
+
 
 
 
