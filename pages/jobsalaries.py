62baa96b53526ed642511d1317fc5a68c999b9db
Charlie <143125248+CarlosVillatoro1995@users.noreply.github.com>
2025-07-10 19:36:45 -0400

    updated jobsalaries html file and python file to obtain the json file

--- pages/jobsalaries.py
+++ pages/jobsalaries.py
@@ -1,7 +1,38 @@
-from flask import Blueprint
+from flask import Blueprint, request, render_template
+import requests
+from dotenv import load_dotenv
+import os #built in module that allows communication between the OS
+from pprint import pprint
+
+load_dotenv() #loads the .env file 
+API_KEY = os.getenv("API_KEY")
 
 jobsalaries_bp = Blueprint("jobsalaries", __name__)
 
-# @jobsalaries_bp.route("/jobsalaries")
-# def job_salary():
+@jobsalaries_bp.route("/jobsalaries")
+def job_salary():
 
+    #Reads the data from the html form the user sends the backend
+    salary_job_title = request.args.get("salary-job-title")
+    salary_location = request.args.get("salary-location")
+
+    url = "https://jsearch.p.rapidapi.com/estimated-salary"
+
+    params = {"job_title": salary_job_title, "location": salary_location}
+
+    headers = {"x-rapidapi-key": API_KEY, "x-rapidapi-host": "jsearch.p.rapidapi.com"}
+
+    response = requests.get(url, params=params, headers=headers) #response from the server with data
+
+    data = response.json() #converts data to json
+
+    pprint(data)
+
+    return render_template("jobsalaries.html", salaries = data["data"]) #renders the html file with
+
+    
+
+    
+
+
+    
