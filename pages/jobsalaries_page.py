9a9d2d156e788647efab41c5a6aafa9dc5166b04
CarlosVillatoro <a7xcharlie18@gmail.com>
2025-08-11 22:39:29 -0400

    Adding the top salaries to the jobsalaries page

--- pages/jobsalaries_page.py
+++ pages/jobsalaries_page.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, render_template
+import pandas as pd
 
 job_salaries_bp = Blueprint("jobsalaries", __name__)
 
 @job_salaries_bp.route("/jobsalaries")
 def job_salaries():
+
+    df = pd.read_excel("data/wages_data_by_state.xlsx")
+
+    
+
     return render_template("jobsalaries_page.html")
