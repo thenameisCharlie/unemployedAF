dafac4fd59cfdbb65d0d9265e588f7727079c7ff
CarlosVillatoro <a7xcharlie18@gmail.com>
2025-08-13 22:15:55 -0400

    Removed the AREA_TITLE column. Won't be visible for top salaries

--- pages/jobsalaries_page.py
+++ pages/jobsalaries_page.py
@@ -14,6 +14,6 @@ def job_salaries():
     top_wages_data = df[df["A_MEAN"] > 100000]
 
     #Narrows the DataFrame to just the columns that are needed
-    top_wages = top_wages_data[["OCC_TITLE", "AREA_TITLE","A_MEAN"]].to_dict(orient="records")
+    top_wages = top_wages_data[["OCC_TITLE","A_MEAN"]].to_dict(orient="records")
 
     return render_template("jobsalaries_page.html", top_wages=top_wages)
