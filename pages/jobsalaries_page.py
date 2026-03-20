6a503d59527a8234375c95ee9d99471a0b985ad4
CarlosVillatoro <a7xcharlie18@gmail.com>
2026-03-19 22:00:07 -0400

    updated name of column for better coding readability

--- pages/jobsalaries_page.py
+++ pages/jobsalaries_page.py
@@ -8,13 +8,16 @@ def job_salaries():
 
     #Read the excel sheet
     df = pd.read_excel("data/wages_data_by_state.xlsx")
+
+    job_salary = df["A_MEAN"]
     
     #converts the column to numeric values and coerce is to ensure no NaN results crashes the code.
-    df["A_MEAN"] = pd.to_numeric(df["A_MEAN"], errors="coerce")
+    #A_MEAN column is the salary of positions
+    job_salary = pd.to_numeric(job_salary, errors="coerce")
 
     #Used a mask to turn the list/Series of True/False. Keep the rows that meet the condition (True)
     #This still returns all the filtered columns where the rows meet the condition
-    top_wages_data = df[df["A_MEAN"] > 150000]
+    top_wages_data = df[job_salary > 150000]
 
     #Retruns 6 rows of the DataFrame and randomizes them 
     randomized_top_wages = top_wages_data.sample(n=6, random_state=None)
